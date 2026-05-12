6b3e6918e1b9005fc770c53c06db42dde267b2ac
firmantomauludani88 <firmantomauludani88@gmail.com>
2026-05-12 09:10:28 +0700

    menambahkan file UL

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolorem animi, tempore voluptatibus repellendus adipisci amet libero neque vero provident aliquam facilis autem asperiores odit, necessitatibus possimus dignissimos, maiores dolores?</p>
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>
+
 </body>
 </html>
