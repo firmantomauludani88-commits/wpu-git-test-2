955cb2476693f764c99e18ff0f20550082df6ccc
firmantomauludani88 <firmantomauludani88@gmail.com>
2026-05-12 09:01:47 +0700

    menambahkan lorem file index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dolorem animi, tempore voluptatibus repellendus adipisci amet libero neque vero provident aliquam facilis autem asperiores odit, necessitatibus possimus dignissimos, maiores dolores?</p>
 </body>
 </html>
